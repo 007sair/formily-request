4e048501b034a3fa1cc2c58073975ec1a5af3568
longchan <longchan@jd.com>
2025-03-24 21:20:26 +0800

    ci(*): update ci config

---
+++ .changeset/chubby-rabbits-fetch.md
@@ -0,0 +1,6 @@
+---
+"formily-request": major
+"site": major
+---
+
+版本 v1.0 重构升级
--- packages/site/.dumirc.ts
+++ packages/site/.dumirc.ts
@@ -12,4 +12,6 @@ export default defineConfig({
   alias: {
     '@site': resolve(__dirname, '.'),
   },
+  base: '/formily-request/',
+  publicPath: '/formily-request/',
 });
